a3be264207d755cba0bcbefe4f61b4b2da43d5c4
Longer <15013044544@163.com>
2022-03-12 15:36:13 +0800

    update

--- 02.cpp
+++ 02.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int num_count = 0;
 
-void MergeAndCount(int array[], int left, int middle, int right) {
+void MergeListAndCount(int array[], int left, int middle, int right) {
 	int* temp = new int[right - left + 5]; //用来存放局部归并后的结果，之后赋予原数组
 	int i = left, j = middle + 1;
 	int k = 0;  // 标定temp的下标和数量。
@@ -17,8 +17,10 @@ void MergeAndCount(int array[], int left, int middle, int right) {
 		else {
 			/* 记录每两个(子)序列归并时的逆序数 */
 			num_count += middle - i + 1;  // 加上左半中所有大于j位置的数的个数。
+			
+			/*打印所有逆序数*/
 			for (int m = i; m <= middle; m++) {
-				cout << "<" << array[m] << ", " << array[j] << ">" << endl;
+				cout << " <" << array[m] << ", " << array[j] << ">" << endl;
 			}
 
 			temp[k++] = array[j++];
@@ -52,24 +54,24 @@ void MergeSort(int array[], int left, int right) {
 	int middle = (left + right) / 2;
 	MergeSort(array, left, middle);             // 递归对左边进行归并
 	MergeSort(array, middle + 1, right);        // 递归对右边进行归并
-	MergeAndCount(array, left, middle, right);  // 归并
+	MergeListAndCount(array, left, middle, right);  // 归并
 }
 
 int main() {
 
 	int num = 0;
-	cout << "Please input the number of sequence: ";
+	cout << " Please input the number of sequence: ";
 	cin >> num;
 
 	int* sequence = new int[num];  // 长度为num的数组，存序列
 	for (int i = 0; i < num; i++){
-		cout << "please input sequence[" << i << "]: ";
+		cout << " please input sequence[" << i << "]: ";
 		cin >> sequence[i];
 	}
 	
-	cout << endl << "All inverse numbers are as follow: " << endl;
+	cout << endl << " All inverse numbers are as follow: " << endl;
 	MergeSort(sequence, 0, num - 1);  //执行归并
-	cout << "The total number of inverse numbers is: " << num_count << endl;
+	cout << " The total number of inverse numbers is: " << num_count << endl;
 	return 0;
 }
 
